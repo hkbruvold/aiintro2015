9e03252058c77a13997b4ae371fd3a4e38672699
burkongla <stianmichael@gmail.com>
2015-10-15 18:51:55 +0200

    Laget tomt brett

--- ass4/EggBoard.py
+++ ass4/EggBoard.py
@@ -1,3 +1,14 @@
 #! /usr/bin/env python3
 
+class Board:
+    def __init__(self, M, N):
+        self.board = []
+        for row in range(M):
+            temple = []
+            for column in range(N):
+                temple.append(False)
+
+            self.board.append(temple)
+            
+                
 
