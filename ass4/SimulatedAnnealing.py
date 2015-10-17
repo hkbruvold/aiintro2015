1356507c63ce44a56051367ec50524344013ffd7
burkongla <stianmichael@gmail.com>
2015-10-17 16:51:27 +0200

    Endringer
    weeee i simulator

--- ass4/SimulatedAnnealing.py
+++ ass4/SimulatedAnnealing.py
@@ -1,9 +1,31 @@
 #! /usr/bin/env python3
 
 
+from math import exp
 
 class SimulatedAnnealing:
     """ Implementiation of the general algorithm"""
     
-    def __init__(self, temp_func, variations_func):
+    def __init__(self, start_board, temp_func, variation_board, points_func):
+        t=1
+        current = start_board
+        while t == 1:
+            temperature= temp_func(t)
+            if temperature = 0:
+                return current
+            nextt= random.variation_board()
+            evaluation = points_func(variation_board) - points_func(current)
+            if evaluation>0:
+                current = variation_board
+            else:
+                prob = exp(evaluation/temperature)
+                current = variation_board
+                
+                
+                
+            
+            
+            
+        
+        
         
